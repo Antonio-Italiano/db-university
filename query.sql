0bec8aef5cac506ff7bb2875014ff17c4b10e53a
Antonio-Italiano <antoitalia221@gmail.com>
2023-02-14 16:06:59 +0100

    student over 30

--- query.sql
+++ query.sql
@@ -13,6 +13,9 @@ WHERE `cfu` > 10;
 
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT `name`, `surname`, `date_of_birth`
+FROM `students`
+WHERE YEAR(`date_of_birth`) > 1993;
 
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
