6d1de63a20b7581ff232db572576778621a3f132
Antonio-Italiano <antoitalia221@gmail.com>
2023-02-14 16:36:57 +0100

    teachers phone is null

--- query.sql
+++ query.sql
@@ -40,7 +40,9 @@ SELECT COUNT(*) AS 'total number of divisions'
 FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
-
+SELECT `name`, `surname`, `phone`
+FROM `teachers`
+WHERE `phone` IS NULL;
 
 
 -- ------------------------------------------ QUERY CON GROUP BY
