2610e10bb1afc52e263373b0870cc961bc730e14
Antonio-Italiano <antoitalia221@gmail.com>
2023-02-14 17:11:41 +0100

    COURSES BY DEPARTMENT

--- query.sql
+++ query.sql
@@ -64,3 +64,6 @@ FROM `exam_student`
 GROUP BY `exam_id`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT `department_id`, COUNT(*) AS 'COURSES BY DEPARTMENT'
+FROM `degrees`
+GROUP BY `department_id`;
