82e5322f8defc57d2b4396693fb392a39927ad28
Antonio-Italiano <antoitalia221@gmail.com>
2023-02-14 16:40:30 +0100

    ENROLLED STUDENTS FOR YEAR

--- query.sql
+++ query.sql
@@ -49,7 +49,9 @@ WHERE `phone` IS NULL;
 
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-
+SELECT COUNT(*) AS 'ENROLLED STUDENTS', YEAR(`enrolment_date`)
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
