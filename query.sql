c746990ceac615e5b21696de82c5dcf345c6d6a6
Antonio-Italiano <antoitalia221@gmail.com>
2023-02-15 09:54:27 +0100

    EX.3 upgrade

--- query.sql
+++ query.sql
@@ -15,7 +15,7 @@ WHERE `cfu` > 10;
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT `name`, `surname`, `date_of_birth`
 FROM `students`
-WHERE YEAR(`date_of_birth`) > 1993;
+WHERE `date_of_birth` < DATE_SUB(CURDATE(), INTERVAL 30 YEAR);
 
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
